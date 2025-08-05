a2faaabb339d044ed5c77fc9471669b186692acf
Alireza Abdi <alirezaabdi00713@gmail.com>
2025-08-05 22:46:43 +0330

    Link first application with the second one

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -1,53 +1,62 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ModuleFederationPlugin } = require('webpack').container;
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
-  entry: './src/index.tsx',
-  mode: 'development',
+  entry: "./src/index.tsx",
+  mode: "development",
   devServer: {
-    static: './dist',
+    static: "./dist",
     port: 3001,
   },
   output: {
-    publicPath: 'http://localhost:3001/',
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    publicPath: "http://localhost:3001/",
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
+        loader: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+        ],
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'app1',
-      filename: 'remoteEntry.js',
+      name: "app1",
+      filename: "remoteEntry.js",
       exposes: {
-        './App': './src/App',
+        "./App": "./src/App",
       },
-shared: {
-  react: {
-    singleton: true,
-    eager: true,
-    requiredVersion: require('./package.json').dependencies.react,
-  },
-  'react-dom': {
-    singleton: true,
-    eager: true,
-    requiredVersion: require('./package.json').dependencies['react-dom'],
-  }
-}
+      shared: {
+        react: {
+          singleton: true,
+          eager: true,
+          requiredVersion: require("./package.json").dependencies.react,
+        },
+        "react-dom": {
+          singleton: true,
+          eager: true,
+          requiredVersion: require("./package.json").dependencies["react-dom"],
+        },
+      },
     }),
     new HtmlWebpackPlugin({
-      template: 'public/index.html',
+      template: "public/index.html",
     }),
   ],
 };
--- app2/src/App.tsx
+++ app2/src/App.tsx
@@ -1,10 +1,26 @@
- import React from 'react';
+import React from "react";
 
 const App: React.FC = () => {
-  return <div>
-    <h1>App2 Microfrontend</h1>
-  </div>;
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow text-center">
+        <h1 className="text-2xl font-bold text-gray-800 mb-4">
+          خوش آمدی
+        </h1>
+        <p className="text-gray-600 mb-6">
+          شما وارد اپلیکیشن دوم شدید!
+        </p>
+        <button
+          onClick={() => {
+            window.location.href = "http://localhost:3001";
+          }}
+          className="px-4 py-2 bg-indigo-500 text-white rounded hover:bg-indigo-400 transition"
+        >
+          بازگشت به اپ اول
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
-
